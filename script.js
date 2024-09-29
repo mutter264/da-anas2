3feb22ff9fc5031496adb4fa543d4ed4f9b2bf8c
mutter264 <gabymutter10@gmail.com>
2024-09-29 14:54:32 -0300

    Adicionando assinatura no js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const avanca = document.querySelectorAll('.btn-proximo');
-
+//Ana Carolina Lopes 3D 29/09/2024
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
